14537df8db147a94db24756c6f5a8dc0d4b741f6
mauricio123451 <maurisanch199@gmail.com>
2025-07-16 20:12:42 -0600

    se subieron cambios al archivo Ejercicio1.java

--- src/SRP/Ejercicio1.java
+++ src/SRP/Ejercicio1.java
@@ -1,19 +1,43 @@
 
 package SRP;
 
-/**
- *
- * @author Charly Cimino
- * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
- * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
- */
 public class Ejercicio1 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public void createUser(String username, String password) {
+        // SRP SINGLE RESPONSABILITY 
+        //establece que una clase, componente o microservicio debe ser responsable de una sola cosa
+        
+        // Lógica de creación
+        System.out.println("Usuario creado: " + username);
     }
+    
 
+    public void validate(String password){
+        //validacion
+        if(password.length() < 8){
+            System.out.println("la contrasena es muy corta");
+        }
+    }
+    
+    public void createcd(String username, String password){
+    //guardado en el disco
+     System.out.println("guardado exitosamente en el disco al usuario"+username);
+    }
+ 
+    /*
+        codigo antiguo
+    
+     public void createUser(String username, String password) {
+        // Lógica de creación
+        System.out.println("Usuario creado: " + username);
+        
+        // Validación
+        if (password.length() < 8) {
+            System.out.println("La contraseña es muy corta");
+        }
+
+        // Guardado en archivo
+        System.out.println("Guardando usuario en disco...");
+    }
+    */
 }
